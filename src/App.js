6fb70703279b84a9fb8053e279a50970c47580fa
derrick matheny <mathenyderrick@gmail.com>
2021-10-10 17:29:41 -0400

    finished up header display

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-
 import { BrowserRouter as Router,Switch,Route } from 'react-router-dom';
 import './App.css';
+import Header from './components/Header';
 import Login from './components/Login';
+
 function App() {
   return (
     <div className="App">
       <Router>
         <Switch>
           <Route exact path='/'>
+            <Header/>
             <Login/>
           </Route>
         </Switch>
--- src/components/Login.js
+++ src/components/Login.js
@@ -5,7 +5,7 @@ const Login = (props) => {
         <Content>
           <CTA>
             <CTALogoOne src="/images/cta-logo-one.svg" alt="" />
-            <SignUp>GET ALL THERE</SignUp>
+            <SignUp>GET ALL THREE</SignUp>
             <Description>
               Get Premier Access to Raya and the Last Dragon for an additional fee
               with a Disney+ subscription. As of 03/26/21, the price of Disney+
