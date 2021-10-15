821a486a4b7537afb94910c16e549d5a231f92f5
derrick matheny <mathenyderrick@gmail.com>
2021-10-14 20:33:08 -0400

    I've tried copying my prevoous code from where the videos were working but even that isnt fixing the disney videos not autoplaying on hover

--- src/components/Viewers.js
+++ src/components/Viewers.js
@@ -6,33 +6,34 @@ const Viewers = (props) => {
             <Wrap>
                 <img src="/images/viewers-disney.png" alt="" />
                 <video autoPlay={true} loop={true} playsInline={true}>
-                    <source src="/videos/1564674844-disney.mp4" type="video/mp4" />
+                    <source src="/videos/1564674844-disney.mp4" type="video/mp4"/>
                 </video>
             </Wrap>
             <Wrap>
                 <img src="/images/viewers-pixar.png" alt="" />
                 <video autoPlay={true} loop={true} playsInline={true}>
-                    <source src="/videos/1564676714-pixar.mp4" type="video/mp4" />
+                    <source src="/videos/1564676714-pixar.mp4" type="video/mp4"/>
                 </video>
             </Wrap>
             <Wrap>
                 <img src="/images/viewers-marvel.png" alt="" />
                 <video autoPlay={true} loop={true} playsInline={true}>
-                    <source src="/videos/1564676115-marvel.mp4" type="video/mp4" />
+                    <source src="/videos/1564676115-marvel.mp4" type="video/mp4"/>
                 </video>
             </Wrap>
             <Wrap>
                 <img src="/images/viewers-starwars.png" alt="" />
                 <video autoPlay={true} loop={true} playsInline={true}>
-                    <source src="/videos/1608229455-star-wars.mp4" type="video/mp4" />
+                    <source src="/videos/1608229455-star-wars.mp4" type="video/mp4"/>
                 </video>
             </Wrap>
             <Wrap>
                 <img src="/images/viewers-national.png" alt="" />
                 <video autoPlay={true} loop={true} playsInline={true}>
-                    <source src="/videos/1564676296-national-geographic.mp4" type="video/mp4" />
+                    <source src="/videos/1564676296-national-geographic.mp" type="video/mp4"/>
                 </video>
             </Wrap>
+
         </Container>
     )
 };
@@ -44,10 +45,8 @@ const Container = styled.div`
     grid-gap:25px;
     gap:25px;
     grid-template-columns: repeat(5, minmax(0,1fr));
-
     @media (max-width:768px) {
         grid-template-columns: repeat(1, minmax(0,1fr));
-
     }
 `
 const Wrap = styled.div`
@@ -60,7 +59,6 @@ const Wrap = styled.div`
   position: relative;
   transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
   border: 3px solid rgba(249, 249, 249, 0.1);
-  
   img {
     inset: 0px;
     display: block;
@@ -73,7 +71,6 @@ const Wrap = styled.div`
     z-index: 1;
     top: 0;
   }
-
   video {
     width: 100%;
     height: 100%;
@@ -81,17 +78,15 @@ const Wrap = styled.div`
     top: 0px;
     opacity: 0;
     z-index: 0;
-  }
-
-  &:hover {
-    box-shadow: rgb(0 0 0 / 80%) 0px 40px 58px -16px,
-      rgb(0 0 0 / 72%) 0px 30px 22px -10px;
-    transform: scale(1.05);
-    border-color: rgba(249, 249, 249, 0.8);
+}
+&:hover {
+box-shadow: rgb(0 0 0 / 80%) 0px 40px 58px -16px,
+    rgb(0 0 0 / 73%) 0px 30px 22px -10px;
+transform: scale(1.05);
+border-color:rgba(249, 249, 249, 0.1);
     video {
-      opacity: 1;
+        opacity:1;
     }
-  }
-`;
-
+}
+`
 export default Viewers
